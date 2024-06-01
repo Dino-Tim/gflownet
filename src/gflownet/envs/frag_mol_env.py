13eae9aecc4fa2299a0f28493353d84a4fd5fa78
Dino-Tim <ok7747706@gmail.com>
2024-06-01 17:38:16 +0530

    Update frag_mol_env.py

--- src/gflownet/envs/frag_mol_env.py
+++ src/gflownet/envs/frag_mol_env.py
@@ -467,9 +467,9 @@ class NCounter:
 def _recursive_decompose(ctx, m, all_matches, a2f, frags, bonds, max_depth=9, numiters=None):
     if numiters is None:
         numiters = [0]
-    numiters[0] += 1
-    if numiters[0] > 10000:
-         return None
+    # numiters[0] += 1
+    # if numiters[0] > 10000:
+    #      return None
     if max_depth == 0 or len(a2f) == m.GetNumAtoms():
         # try to make a mol, does it work?
         # Did we match all the atoms?
